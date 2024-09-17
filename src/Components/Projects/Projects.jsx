359a29d7d4685f3909ff0cc4ca5d663c377b9cb8
KshitijAdhikaree <adhikareekshitij@gmail.com>
2024-09-18 01:10:13 +0545

    added blog app to products

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,29 +1,31 @@
 import React from "react";
 import "./Projects.css";
-import project_1 from "../../assets/gosolo.jpg";
+import gosolo from "../../assets/gosolo.jpg";
 import comingsoon from "../../assets/comingsoon.jpg";
+import odysseyopus from "../../assets/odysseyopus.png";
 
 import gosolo_logo from "../../assets/gosolologo.png";
 import comingsoon_icon from "../../assets/comingsoon-icon.png";
+import odysseyopus_logo from "../../assets/odysseyopus_logo.png";
 
 const Projects = () => {
   return (
     <div className="projects">
-      <a href="https://www.gosolo.com" target="_blank">
+      <a href="https://www.gtps.me" target="_blank">
         <div className="project">
-          <img src={project_1} alt="project1" />
+          <img src={gosolo} alt="GTPS" />
           <div className="caption">
             <img src={gosolo_logo} alt="projectIcon1" />
-            <p>Go Solo: Travel Website</p>
+            <p>GTPS: Travel App</p>
           </div>
         </div>
       </a>
-      <a href="https://www.notfound.com" target="_blank">
+      <a href="https://www.odysseyopus.com" target="_blank">
         <div className="project">
-          <img src={comingsoon} alt="project2" />
+          <img src={odysseyopus} alt="project2" />
           <div className="caption">
-            <img src={comingsoon_icon} alt="projectIcon2" />
-            <p>Coming Soon</p>
+            <img src={odysseyopus_logo} alt="projectIcon2" />
+            <p>Odyssey Opus: Blog App</p>
           </div>
         </div>
       </a>
